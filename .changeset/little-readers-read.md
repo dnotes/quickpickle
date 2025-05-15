186848c22b96ce431603f743d9559239c96d903c
David Hunt <git@dnotes.net>
2025-05-15 16:46:26 +1200

    fix: types and imports for browser package

---
+++ .changeset/little-readers-read.md
@@ -0,0 +1,5 @@
+---
+"@quickpickle/vitest-browser": patch
+---
+
+fix types and frameworks imports, and remove old file
--- packages/browser/world.ts
+++
@@ -1,7 +0,0 @@
-import { setWorldConstructor } from "quickpickle";
-import { BrowserWorld } from "./BrowserWorld";
-
-
-setWorldConstructor(VitestBrowserWorld);
-
-export { VitestBrowserWorld };
